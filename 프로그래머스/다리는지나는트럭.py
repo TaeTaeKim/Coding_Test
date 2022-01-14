7508ae15b805b31cb41655632729f1fcefa81a2f
TaeTae <hc07car@gmail.com>
2022-01-14 18:44:43 +0900

    다리를 지나느 트럭 푸는중

--- 프로그래머스/다리는지나는트럭.py
+++ 프로그래머스/다리는지나는트럭.py
@@ -1,3 +1,15 @@
+from collections import deque
 def solution(bridge_length, weight, truck_weights):
+    trucks= deque([(i,0) for i in truck_weight])
+    print(trucks)
+    onbridge = []
+    while len(onbridge)==len(t)
     answer = 0
     return answer
+
+
+bridge_length = 2
+weight = 10
+truck_weight = [7,4,5,6]
+
+print(solution(bridge_length,weight,truck_weight))
